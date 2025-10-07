dbd45ed0fc0c5429ea4724063105c8ff1df9ff39
Yougal kumar <kyougal70@gmail.com>
2025-10-07 13:43:07 +0530

    add cron

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
 
 @Controller('card')
 export class UsersController {
@@ -10,6 +12,18 @@ export class UsersController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
+
+  @Post('hello')
+  test() {
+    return 'test';
+  }
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async continueRequest() {
+    await axios('https://get-details-a8r0.onrender.com/hello');
+  }
+
+  // https://get-details-a8r0.onrender.com
 
   @Get()
   findAll() {
